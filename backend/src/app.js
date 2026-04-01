0b6ca00aad4d3d6d5f1e67eb3d224bef98721c5e
Manoj Lamani <manoj.p24@medhaviskillsuniversity.edu.in>
2026-04-02 01:07:44 +0530

    Setup Express server

---
+++ backend/src/app.js
@@ -0,0 +1,25 @@
+import express from "express";
+import cors from "cors";
+
+import healthRoutes from "./routes/health.js";
+import authRoutes from "./routes/authRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
+import projectRoutes from "./routes/projectRoutes.js";
+import taskRoutes from "./routes/taskRoutes.js";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api", healthRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/projects", projectRoutes);
+app.use("/api/tasks", taskRoutes);
+
+app.get("/", (req, res) => {
+    res.send("Backend server is running");
+});
+
+export default app;
